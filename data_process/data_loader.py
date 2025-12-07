760ab6a6a386dc54631afbebe391111bf4c5f4e5
itu-itis24-seyfullayev24 <seyfullayev24@itu.edu.tr>
2025-12-07 14:35:37 +0300

    added some changes to data_loader again

--- data_process/data_loader.py
+++ data_process/data_loader.py
@@ -1,21 +1,26 @@
+import os
 import pandas as pd
 import torch
-from torch.utils.data import Dataset
-import os
+from torch.utils.data import Dataset, DataLoader
+
 
 
 class DatasetReader(Dataset):
-    def __init__(self, folder_path, selected_emotions):
+    def __init__(self, folder_path, selected_emotions=None):
         self.folder_path = folder_path
-        self.selected = set(selected_emotions)
+        self.selected = set(selected_emotions) if selected_emotions else None
         self.files = []
 
         for fname in os.listdir(folder_path):
-            if fname.endswith(".txt"):
-                emotion_id = int(fname.split("_")[1].split(".")[0])
+            if not fname.endswith(".txt"):
+                continue
 
-                if emotion_id in self.selected:
-                    self.files.append(os.path.join(folder_path, fname))
+
+            parts = fname.replace(".txt", "").split("_")
+            emotion_id = int([p for p in parts if p.isdigit()][0])
+
+            if self.selected is None or emotion_id in self.selected:
+                self.files.append(os.path.join(folder_path, fname))
 
     def __len__(self):
         return len(self.files)
@@ -24,39 +29,10 @@ class DatasetReader(Dataset):
         full_path = self.files[idx]
 
         df = pd.read_csv(full_path, sep="\t", header=None)
-        data_np = df.values
+        eeg = torch.tensor(df.values, dtype=torch.float32).T  # (channels, samples)
 
-        eeg = torch.tensor(data_np, dtype=torch.float32).T
 
         fname = os.path.basename(full_path)
-        emotion_id = int(fname.split("_")[1].split(".")[0])
+        emotion_id = int([p for p in fname.split("_") if p.split(".")[0].isdigit()][0])
 
         return eeg, emotion_id
-
-if __name__ == "__main__":
-    from torch.utils.data import DataLoader
-    from wavelet import WaveletModule
-
-    dataset = DatasetReader(
-        folder_path="eeg_raw",
-    )
-
-    loader = DataLoader(dataset, batch_size=1, shuffle=True)
-
-    wavelet = WaveletModule()
-    for eeg, emotion in loader:
-
-        eeg = eeg.squeeze(0)
-        print("eeg shape:", eeg.shape)
-        print("emotion:", emotion.item())
-
-
-        windows = wavelet.create_windows(eeg)
-        print("windows:", windows.shape)
-
-        coeffs = wavelet.wavelet(windows[0])
-        print("coeffs:", coeffs.shape)
-
-        bandpowers = wavelet.bandpowers_wavelet(coeffs)
-        print("bandpowers:", bandpowers.shape)
-        break
