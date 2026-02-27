8fe24bd9ce1966aad4f69f0595b76220c14016ab
Elnur <thegamercoder19@gmail.com>
2026-02-28 01:47:35 +0400

    MobileNet with Atrocious Convolutions

--- src/snn_modeling/layers/residual_blocks.py
+++ src/snn_modeling/layers/residual_blocks.py
@@ -5,9 +5,27 @@ from torchvision.ops import StochasticDepth
 
 
 class ConvSpiking(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False, spike_model=snn.Leaky, use_norm = False, **neuron_params):
+    def __init__(self, in_channels, 
+                 out_channels, 
+                 kernel_size, 
+                 stride=1, 
+                 padding=0, 
+                 groups=1,
+                 dilation=0, 
+                 bias=False, 
+                 spike_model=snn.Leaky, 
+                 use_norm = False, 
+                 **neuron_params):
         super(ConvSpiking, self).__init__()
-        self.conv = TimeDistributed(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias, groups=groups))
+        #pad = dilation * (kernel_size - 1) // 2
+        self.conv = TimeDistributed(nn.Conv2d(in_channels, 
+                                              out_channels, 
+                                              kernel_size=kernel_size, 
+                                              stride=stride, 
+                                              padding=padding,
+                                              dilation=dilation, 
+                                              bias=bias, 
+                                              groups=groups))
         layer_params = neuron_params.copy()
         if spike_model.__name__ == 'ALIF':
             layer_params['num_channels'] = out_channels
@@ -23,20 +41,29 @@ class ConvSpiking(nn.Module):
         return x
 
 class SpikingResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, p_drop=0.2, p_path=0.1, spike_model=snn.Leaky, use_norm = False, **neuron_params):
+    def __init__(self, 
+                 in_channels, 
+                 out_channels, 
+                 stride=1, 
+                 dilation=1, 
+                 p_drop=0.2, 
+                 p_path=0.1, 
+                 spike_model=snn.Leaky, 
+                 use_norm = False, 
+                 **neuron_params):
+        
         super(SpikingResBlock, self).__init__()
-
-        groups = in_channels if in_channels == out_channels else 1
 
         self.block1 = ConvSpiking(
             in_channels, 
             out_channels, 
             kernel_size=3, 
             stride=stride, 
-            padding=1, 
+            padding=dilation,
+            dilation=dilation,
             bias=False, 
             spike_model=spike_model, 
-            groups=groups,
+            groups=in_channels,
             use_norm=use_norm,
             **neuron_params
         )
@@ -44,8 +71,7 @@ class SpikingResBlock(nn.Module):
         self.block2 = ConvSpiking(
             out_channels, 
             out_channels, 
-            kernel_size=3, 
-            padding=1, 
+            kernel_size=3,  
             bias=False, 
             spike_model=nn.Identity, 
             use_norm=use_norm,
@@ -55,9 +81,16 @@ class SpikingResBlock(nn.Module):
         self.drop = TemporalOrderFix(nn.Dropout3d(p=p_drop))
 
         if stride != 1 or in_channels != out_channels:
-            self.downsample = ConvSpiking(in_channels, out_channels, kernel_size=1, stride=stride, bias=True, spike_model=nn.Identity, use_norm=True) #TimeDistributed(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=True))
+            self.downsample = ConvSpiking(in_channels, 
+                                          out_channels, 
+                                          kernel_size=1, 
+                                          stride=stride, 
+                                          bias=False, 
+                                          spike_model=nn.Identity, 
+                                          use_norm=True)
         else:
             self.downsample = nn.Identity()
+
         layer_params = neuron_params.copy()
         if spike_model.__name__ == 'ALIF':
             layer_params['num_channels'] = out_channels
--- src/snn_modeling/models/encoders.py
+++ src/snn_modeling/models/encoders.py
@@ -7,6 +7,24 @@ class SpikingResNet18Encoder(nn.Module):
     def __init__(self, in_channels, p_drop=0.1, vit_p_drop=0.25, vit=False, gc=False, spike_model=snn.Leaky, **neuron_params):
         super(SpikingResNet18Encoder, self).__init__()
 
+
+        def _make_stage(in_c, out_c, stride, dilation, blocks, **params):
+            layers = []
+            layers.append(SpikingResBlock(in_c, 
+                                          out_c, 
+                                          stride=stride,
+                                          p_drop=p_drop,
+                                          spike_model=spike_model,
+                                          **params))
+            for _ in range(1, blocks):
+                layers.append(SpikingResBlock(out_c, 
+                                              out_c, 
+                                              dilation=dilation,
+                                              p_drop=p_drop,
+                                              spike_model=spike_model,
+                                              **params))
+            return nn.Sequential(*layers)
+
         self.vit = vit
         self.stem = ConvSpiking(
             in_channels,
@@ -23,6 +41,17 @@ class SpikingResNet18Encoder(nn.Module):
         if spike_model.__name__ == 'ALIF':
             no_norm_layer_params['batch_norm'] = False
 
+        last_layer_params = no_norm_layer_params.copy()
+        if spike_model.__name__ == 'ALIF':
+            last_layer_params['return_mem'] = True
+
+        self.stage1 = _make_stage(20, 32, stride=2, dilation=1, blocks=2, **no_norm_layer_params)
+        self.stage2 = _make_stage(32, 64, stride=2, dilation=1, blocks=3, **no_norm_layer_params)
+        self.stage3 = _make_stage(64, 128, stride=2, dilation=2, blocks=3, **no_norm_layer_params)
+        self.stage4 = _make_stage(128, 256, stride=2, dilation=4, blocks=2, **last_layer_params)
+
+        '''
+
         self.layer1a = SpikingResBlock(20, 32, p_drop=p_drop, use_norm = True, spike_model=spike_model, **no_norm_layer_params)
         self.layer1b = SpikingResBlock(32, 32, p_drop=p_drop, use_norm = True, spike_model=spike_model, **no_norm_layer_params)
         
@@ -32,12 +61,11 @@ class SpikingResNet18Encoder(nn.Module):
         self.layer3a = SpikingResBlock(64, 128, p_drop=p_drop, use_norm = True, stride=2, spike_model=spike_model, **no_norm_layer_params)
         self.layer3b = SpikingResBlock(128, 128, p_drop=p_drop, use_norm = True, spike_model=spike_model, **no_norm_layer_params)
 
-        last_layer_params = no_norm_layer_params.copy()
-        if spike_model.__name__ == 'ALIF':
-            last_layer_params['return_mem'] = True
+        
   
         self.layer4a = SpikingResBlock(128, 128, p_drop=p_drop, use_norm = True, stride=1, spike_model=spike_model, **last_layer_params)
         self.layer4b = SpikingResBlock(128, 128, p_drop=p_drop, use_norm = True, spike_model=spike_model, **last_layer_params)
+        '''
 
         if vit:
             self.temporal = TemporalViTBlock(512, num_heads=8, p_drop=vit_p_drop)
@@ -48,6 +76,7 @@ class SpikingResNet18Encoder(nn.Module):
 
     def forward(self, x):
         x = self.stem(x)
+        '''
         s1 = self.layer1a(x)
         s1 = self.layer1b(s1)
         s2 = self.layer2a(s1)
@@ -57,6 +86,14 @@ class SpikingResNet18Encoder(nn.Module):
         s4 = self.layer4a(s3)
         s4 = self.layer4b(s4)
         #s4 = self.temporal(s4)
+        '''
+
+        s1 = self.stage1(x)
+        s2 = self.stage2(s1)
+        s3 = self.stage3(s2)
+        s4 = self.stage4(s3)
+        s4 = self.temporal(s4)
+        
         return s4, [s1, s2, s3]
     
 
