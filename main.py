e790a2aee215d0151d039c9b06d607f66bcbf0c1
Elnur <thegamercoder19@gmail.com>
2026-04-13 20:27:37 +0400

    Update

--- main.py
+++ main.py
@@ -286,14 +286,14 @@ def main():
             if isinstance(model, SpikingResNetClassifier):
                 enc_class = model
             else:
-            encoder_dict = getattr(config, 'encoder', {})
-            use_bn = encoder_dict.get('use_batchnorm', False) if isinstance(encoder_dict, dict) or hasattr(encoder_dict, 'get') else False
-            enc_class = SpikingResNetClassifier(
-                encoder_backbone = model.encoder,
-                num_classes=config.model.get('num_classes', 5),
-                use_swiglu=config.model.get('use_swiglu', False),
-                use_batchnorm=use_bn
-            ).to(device)
+                encoder_dict = getattr(config, 'encoder', {})
+                use_bn = encoder_dict.get('use_batchnorm', False) if isinstance(encoder_dict, dict) or hasattr(encoder_dict, 'get') else False
+                enc_class = SpikingResNetClassifier(
+                    encoder_backbone = model.encoder,
+                    num_classes=config.model.get('num_classes', 5),
+                    use_swiglu=config.model.get('use_swiglu', False),
+                    use_batchnorm=use_bn
+                ).to(device)
             
             enc_class.load_state_dict(checkpoint['model_state_dict'])
             find_representative_subject(enc_class, config, device, samples_per_subject=500)
